fe3c89aea9e5b89ca02b2aae1a13b22ecf400b47
MichelSSantos <michelshaiene2010@hotmail.com>
2023-02-02 11:18:37 -0300

    Project created

---
+++ src/application/ProgramChess.java
@@ -0,0 +1,9 @@
+package application;
+
+public class ProgramChess {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author michel
+ *
+ */
+module chessSystem {
+}
